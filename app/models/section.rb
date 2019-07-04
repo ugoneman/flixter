4a49d4a5732ebb488e449bf8fd7d9f58c0fb7dfb
Ugo Otuonye <ugoneman@gmail.com>
2019-07-04 00:34:21 +0000

    Flixter lesson 33

--- app/models/section.rb
+++ app/models/section.rb
@@ -2,6 +2,16 @@ class Section < ApplicationRecord
   belongs_to :course
   has_many :lessons
 
+  def next_section
+    section = course.sections.where("row_order > ?", self.row_order).rank(:row_order).first
+    return section
+  end
+
+  def previous_section
+    section = course.sections.where("row_order < ?", self.row_order).rank(:row_order).last
+    return section
+  end
+              
   include RankedModel
   ranks :row_order, with_same: :course_id
 end
