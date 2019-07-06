0f85d003183ad6c3d3233c9c26a8f7a929900924
Ugo Otuonye <ugoneman@gmail.com>
2019-07-06 19:23:28 +0000

    lesson 35

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -16,7 +16,8 @@ class Instructor::CoursesController < ApplicationController
   end
 
   def show
-    @course = Course.find(params[:id])
+    #@course = Course.find(params[:id])
+    @section = Section.new
   end
 
   private
